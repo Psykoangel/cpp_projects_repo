e772ddd70f9e2dd17b5240ba769f714c1cbb3d1d
Michaël Scherer <michael.scherer@viacesi.fr>
2016-02-04 00:09:36 +0100

    Other tranning algorithms I found on old backups. These training files were one of the first tests I built with the Boost library

---
+++ Training/Algorithm/djikstra.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<vector>
+//not using namespace std!!
+
+class graph
+{
+    /*holds adjacency matrix as vector
+    provides interface to access 1d vector as a 2d vector
+    and much more
+    */
+    std::vector<int> gmat;
+    const int N;
+
+  public:
+    graph(int n): N(n)
+    {
+        gmat = std::vector<int>(N*N);
+    }
+
+    int& operator()(unsigned int r, unsigned int c)
+    {
+        return gmat[(r-1)*N+c-1];
+    }
+
+    int& operator[](unsigned int key)
+    { 
+        return gmat[key];
+    }
+};
+
+struct history
+{
+    int len, prev;
+};
+
+int main()
+{
+
+    int N; std::cin>>N;
+    graph data(N);
+
+    for (int i=0; i<N*N; i++)//read the adj. matrix
+      std::cin>>data[i];
+    //std::cout<<data(1, 1)<<std::endl<<data(2, 2);
+
+    int sn, en, visited_size = 0;//as soon as visited size==N, break
+
+    //answer is a pair of {len, previous}
+    history answer[N];
+
+    //visited is a BitArray. If vertex 4,2 are visited then
+    //visited == {0,0,1,0,1,...}
+    //visited means fully checked (compare with wikipedia algo)
+    int visited[N];
+
+    //initialising
+    for (int i=0; i<N; i++)
+    {
+          answer[i].len = -1; //equivalent to infinity
+          visited[i] = 0;
+    }
+
+    cin>>sn>>en;
+    //start the algo!
+    answer[i] = 0;
+
+    int curn=sn, new_len;
+
+    while(visited_size <= N)
+    {
+        for (int i=0; i<N; i++)
+        {
+            if (!visited[i] && data(curn, i) >= 0) //!visited and edge exists
+            {
+                new_len = answer[curn].len + data(curn, i);
+                
+                if (new_len < answer[i].len || answer[i] == -1) //new_len < old_len OR old_len == infinity
+                {
+                    answer[i].len = new_len;
+                    answer[i].prev = curn;
+                }
+                //else do nothing
+            }
+        }
+
+        visited_size++;
+        visited[curn] = 1;
+        //select new node
+    }
+
+      return 0;
+  }
---
+++ Training/Boost_test/Boost_Pointers.cpp
@@ -0,0 +1,200 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+#include <windows.h>
+#include <typeinfo>
+#include <ctime>
+#include <limits>
+// for intrusive_ptr
+#include <atlbase.h>
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/scoped_array.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/chrono.hpp>
+#include <boost/chrono/duration.hpp>
+#include <boost/optional.hpp>
+#include <boost/intrusive_ptr.hpp>
+
+//#define BOOST_SP_USE_QUICK_ALLOCATOR
+
+namespace bcr = boost::chrono;
+
+#ifdef max
+#undef max
+#endif
+
+template <class T, size_t N>
+size_t array_size(T(&)[N]) { return N; }
+
+struct DummyAddRef
+{
+	void AddRef(){}
+	void Release(){}
+};
+
+// for intrusive_ptr
+void intrusive_ptr_add_ref(IDispatch *p) { p->AddRef(); }
+void intrusive_ptr_release(IDispatch *p) { p->Release(); }
+
+void intrusive_ptr_add_ref(DummyAddRef *p) { p->AddRef(); }
+void intrusive_ptr_release(DummyAddRef *p) { p->Release(); }
+
+class CoInitializeGuard
+{
+public:
+	CoInitializeGuard(){ CoInitialize(0); }
+	~CoInitializeGuard(){ CoUninitialize(); }
+};
+
+namespace boost_test
+{
+	class Timer
+	{
+		boost::optional<bcr::system_clock::time_point> m_startTime;
+		boost::optional<bcr::system_clock::duration> m_timeDuration; ///tick is 100 nanoseconds
+	public:
+		void Start()
+		{
+			m_startTime.reset(bcr::system_clock::now());
+		}
+		void Stop()
+		{
+			auto curTime = bcr::system_clock::now();
+			assert(m_startTime);
+			
+			if (m_startTime)
+				m_timeDuration.reset(curTime - *m_startTime);
+			else
+			{
+				assert(false);
+				m_timeDuration.reset();
+			}
+		}
+
+		double DurationInInSec() const
+		{
+			auto res = DurationInInMicroSec();
+			return res > 0 ? res / 1000000.0 : res;
+		}
+
+		__int64 DurationInInMicroSec() const
+		{
+			if (!m_timeDuration)
+			{
+				assert(false);
+				return -1;
+			}
+
+			bcr::microseconds usec = bcr::duration_cast<bcr::microseconds>(*m_timeDuration);
+			return (usec <= bcr::microseconds(0)) ? 0 : usec.count();
+		}
+	};
+	void TestScopedPtr()
+	{
+		std::cout << "TestScopedPtr" << std::endl;
+
+		boost::scoped_ptr<int> p{ new int{ 1 } };
+		std::cout << *p << std::endl;
+		p.reset(new int{ 2 });
+		std::cout << *p.get() << std::endl;
+		p.reset();
+		std::cout << std::boolalpha << static_cast<bool>(p) << std::endl;
+	}
+
+	void TestScopedArray()
+	{
+		std::cout << "TestScopedArray" << std::endl;
+		enum e_ar_count
+		{
+			ar_count = 2
+		};
+
+		boost::scoped_array<int> ar_p{ new int[ar_count] };
+		*ar_p.get() = 2;
+		ar_p[1] = 3;
+
+		for (auto i = 0; i < ar_count; ++i)
+			std::cout << ar_p[i] << std::endl;
+	}
+
+	void CloseWindowHandle(HANDLE handle)
+	{
+		CloseHandle(handle);
+	}
+
+	void TestSharedPtr()
+	{
+		std::cout << "TestSharedPtr" << std::endl;
+
+		typedef boost::shared_ptr<int> int_ptr;
+		int_ptr p1{ new int{ 1 } };
+		int_ptr p2 = p1;
+		std::cout << *p1 << std::endl << *p2 << std::endl;
+		p1.reset();
+		std::cout << std::boolalpha << static_cast<bool>(p1) << std::endl;
+		std::cout << std::boolalpha << static_cast<bool>(p2) << std::endl;
+		p2.reset();
+		std::cout << std::boolalpha << static_cast<bool>(p2) << std::endl;
+
+		{
+			boost::shared_ptr<void> processPtr(OpenProcess(PROCESS_SET_INFORMATION, FALSE, GetCurrentProcessId()), CloseWindowHandle);
+			UNREFERENCED_PARAMETER(processPtr);
+		}
+	}
+
+	void TestMakeSharedPtr()
+	{
+		std::cout << "TestMakeSharedPtr" << std::endl;
+
+		auto p1 = boost::make_shared<int>(22);
+		std::cout << typeid(p1).name() << std::endl;
+		auto p2 = boost::make_shared<int[10]>(10);
+		std::cout << typeid(p2).name() << std::endl;
+	}
+
+	void TestIntrusivePtr()
+	{
+		std::cout << "TestIntrusivePtr" << std::endl;
+
+		CoInitializeGuard initialGuard;
+		UNREFERENCED_PARAMETER(initialGuard);
+
+		CLSID clsid;
+		CLSIDFromProgID(CComBSTR{ "Scripting.FileSystemObject" }, &clsid);
+		void *p;
+		CoCreateInstance(clsid, 0, CLSCTX_INPROC_SERVER, __uuidof(IDispatch), &p);
+		boost::intrusive_ptr<IDispatch> disp{ static_cast<IDispatch*>(p), false };
+		CComDispatchDriver dd{ disp.get() };
+		CComVariant arg{ "C:\\Windows" };
+		CComVariant ret{ false };
+		dd.Invoke1(CComBSTR{ "FolderExists" }, &arg, &ret);
+		
+		std::cout << std::boolalpha << (ret.boolVal != 0) << std::endl;
+	}
+}
+
+
+void TestBoost()
+{
+	using namespace boost_test;
+
+	TestScopedPtr();
+	TestScopedArray();
+	TestSharedPtr();
+	TestMakeSharedPtr();
+	TestIntrusivePtr();
+}
+
+int main()
+{
+	TestBoost();
+
+	std::cout << "Press any key to Exit" << std::endl;
+	char ch;
+	std::cin >> ch;
+
+	return 0;
+}
